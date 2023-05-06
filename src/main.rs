8eb5926a9fa80281780ba7d3821a91a4fdaf6566
zenlex <erichrkeil@gmail.com>
2023-05-06 15:55:41 -0500

    userlist complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+use gloo_net::{http::Request, Error};
+use models::user::Users;
+use yew::prelude::*;
+
+use components::{card::Card, header::Header, loader::Loader, message::Message};
+// Components
+mod components;
+mod models;
+#[function_component(App)]
+fn app() -> Html {
+    let users: UseStateHandle<Option<Users>> = use_state(|| None);
+    let error: UseStateHandle<Option<Error>> = use_state(|| None);
+
+    {
+        let users = users.clone();
+        let error = error.clone();
+
+        use_effect_with_deps(
+            move |_| {
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_users = Request::get("https://dummyjson.com/users").send().await;
+                    match fetched_users {
+                        Ok(response) => {
+                            let json = response.json::<Users>().await;
+                            match json {
+                                Ok(json_resp) => users.set(Some(json_resp)),
+                                Err(e) => error.set(Some(e)),
+                            }
+                        }
+                        Err(e) => error.set(Some(e)),
+                    }
+                });
+                || ()
+            },
+            (),
+        )
+    }
+
+    let user_list_logic = match users.as_ref() {
+        Some(users) => users
+            .users
+            .iter()
+            .map(|user| {
+                html! {
+                    <Card user={user.clone()} />
+                }
+            })
+            .collect(),
+        None => match error.as_ref() {
+            Some(_) => {
+                html! {
+                    <Message text={"Error getting list of users"} css_class={"text-danger"}/>
+                }
+            }
+            None => {
+                html! {
+                    <Loader />
+                }
+            }
+        },
+    };
+
+    html! {
+        <>
+            <Header />
+            {user_list_logic}
+        </>
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
